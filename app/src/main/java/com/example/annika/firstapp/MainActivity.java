94be3c6d87ab23637f9d4a7b7e681f528b717dfd
kicka-r <kicka.r@live.se>
2015-10-22 20:17:45 +0200

    Livsråd!

--- app/src/main/java/com/example/annika/firstapp/MainActivity.java
+++ app/src/main/java/com/example/annika/firstapp/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.annika.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ArrayList<String> advices = new ArrayList<>();
+        advices.add("va int dum!");
+        advices.add("lev livet!");
+        advices.add("lev som du lär");
+
+        final Random r = new Random();
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, advices.get(r.nextInt(advices.size()-1)), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
